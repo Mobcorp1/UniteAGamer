d8a166eedf97f5c10fc1987a3701736bf02f843c
Mike <mikemarsh2014@yahoo.com>
2026-06-03 19:29:03 +0100

    fix splash app entry gate import

--- lib/build/splash_screen.dart
+++ lib/build/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:uag_arc_raiders_hub/screens/build/auth/auth_landing_screen.dart';
+import 'package:uag_arc_raiders_hub/screens/build/app_entry_gate.dart';
 import 'package:uag_arc_raiders_hub/widgets/static_watermark.dart';
 import 'package:uag_arc_raiders_hub/widgets/theme.dart';
 
