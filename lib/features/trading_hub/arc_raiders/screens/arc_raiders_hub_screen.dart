12fe099307837df0881302f2a9397d368bc9fbcf
Mike <mikemarsh2014@yahoo.com>
2026-06-23 23:08:10 +0100

    PASS 246 fix referral tools route compile

--- lib/features/trading_hub/arc_raiders/screens/arc_raiders_hub_screen.dart
+++ lib/features/trading_hub/arc_raiders/screens/arc_raiders_hub_screen.dart
@@ -64,7 +64,7 @@ class _ArcRaidersHubScreenState extends State<ArcRaidersHubScreen> {
       accent: AppTheme.neonCyan,
       art: _ArcHubArtKind.smart,
       assetName: 'arc_hub_referral_tools.webp',
-      builder: (_) => const ReferralToolsScreen(),
+      builder: (_) => ReferralToolsScreen(),
     ),
     _ArcHubFeature(
       title: 'Subscriptions',
