d4e10d33359d4cf150e0dd56d7689e5c2d214294
Mike <mikemarsh2014@yahoo.com>
2026-06-23 01:50:20 +0100

    PASS 243 fix widget tests without firebase

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,12 @@
-// Basic widget test for UAG Arc Raiders Hub.
-//
-// This test just verifies that the root app widget builds.
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:uag_arc_raiders_hub/main.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   testWidgets('UAG Arc Raiders Hub app builds', (WidgetTester tester) async {
     await tester.pumpWidget(const UAGTradersHubApp());
+
     expect(find.byType(UAGTradersHubApp), findsOneWidget);
   });
 }
